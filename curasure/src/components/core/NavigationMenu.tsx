05d1eed4b8b0d152aba971f5b1e6e910f54da225
Indorkar, Sanika Rajesh <sindorka@iu.edu>
2025-04-10 17:44:19 -0400

    adding node modules

---
+++ curasure/src/components/core/NavigationMenu.tsx
@@ -0,0 +1,47 @@
+
+
+// const NavigationMenu = () => {
+
+//     return(
+//         <nav className="navbar">
+//               <div className="navbar-left">CuraSure</div>
+//               <div className="navbar-container">
+//                 <ul className="navbar-menu">
+//                   <li
+//                     onClick={() => handleNavClick('hero')}
+//                     className={`nav-item ${activeSection === 'hero' ? 'active' : ''}`}
+//                   >
+//                     Home
+//                   </li>
+//                   <li
+//                     onClick={() => handleNavClick('about')}
+//                     className={`nav-item ${activeSection === 'about' ? 'active' : ''}`}
+//                   >
+//                     About
+//                   </li>
+//                   <li
+//                     onClick={() => handleNavClick('roles')}
+//                     className={`nav-item ${activeSection === 'roles' ? 'active' : ''}`}
+//                   >
+//                     Roles
+//                   </li>
+//                   <li
+//                     onClick={() => handleNavClick('contact')}
+//                     className={`nav-item ${activeSection === 'contact' ? 'active' : ''}`}
+//                   >
+//                     Contact
+//                   </li>
+//                 </ul>
+//               </div>
+//               <div className="navbar-right">
+//                 <button className="login-btn"
+//                   onClick={handleLogin}
+//                 >
+//                   Log In
+//                 </button>
+//               </div>
+//             </nav>
+//     )
+// }
+
+// export default NavigationMenu;
